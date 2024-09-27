2bf5dac5bdcaf75a4f2a28a46d1ff0ed8c8a954e
Elda silva <elda-cris@hotmail.com>
2024-09-26 21:06:22 -0300

    entrada nome do usuario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome");
+        String nome = sc.nextLine();
+
+        sc.close();
+
+        System.out.printf("olá, %s!", nome);
+
     }
 }
